c1e8434fe3f1aa7b7fd7292f643f0ac26e513c0f
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2021-01-02 18:22:18 +0530

    feat: check chore function

--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@ var date = document.querySelector("#bdate");
 var luckyNum = document.querySelector("#luckyn");
 var btn = document.querySelector("#submit");
 
-document.getElementById("output").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+function show() {
+  document.getElementById("output").innerHTML = `
+    <h1>Hello Vanilla!</h1>
+    <div>
+      We use the same configuration as 
+      <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+    </div>
+    `;
+}
+
+function check() {
+  let birthdate = new Date(date);
+  if (birthdate == "Invalid Date") {
+    alert("Please Enter A Valid Date");
+  } else {
+    console.log(date);
+  }
+}
+
+document.addEventListener(btn, check);
