c2534d7b7cd6a216a5869d20763552a44e055d0a
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2021-01-02 17:43:49 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+document.getElementById("output").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
